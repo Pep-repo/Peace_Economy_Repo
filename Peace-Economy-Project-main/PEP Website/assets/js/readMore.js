60daed29088aa537992304c76bd500f85bbe86a5
Christopher C Fleming <63308959+ChristopherCFleming@users.noreply.github.com>
2024-02-07 16:31:56 -0500

    Debug Read More Button

--- Peace-Economy-Project-main/PEP Website/assets/js/readMore.js
+++ Peace-Economy-Project-main/PEP Website/assets/js/readMore.js
@@ -1,12 +1,16 @@
-function toggleText() {
-    var moreText = document.getElementById("more-text");
-    var btnText = document.getElementById("toggleButton");
+function toggleText(button) {
+    // Find the closest article ancestor of the button
+    var article = button.closest('.post');
+    
+    // Within this article, find the .more-text span
+    var moreText = article.querySelector('.more-text');
 
+    // Toggle the display of the .more-text span and button text
     if (moreText.style.display === "none") {
         moreText.style.display = "inline";
-        btnText.textContent = "Read Less"; // Change button text to "Read Less"
+        button.textContent = "Read Less"; // Change button text to "Read Less"
     } else {
         moreText.style.display = "none";
-        btnText.textContent = "Read More"; // Change button text to "Read More"
+        button.textContent = "Read More"; // Change button text to "Read More"
     }
 }
